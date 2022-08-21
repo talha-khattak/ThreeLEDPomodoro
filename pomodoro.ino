9793e1b1bf98d098ee4d1849179b52a36b338537
talha-khattak <109832903+talha-khattak@users.noreply.github.com>
2022-08-21 12:41:15 -0400

    Create pomodoro.ino

---
+++ pomodoro.ino
@@ -0,0 +1,31 @@
+#include <time.h>
+#include <TimeAlarms.h>
+
+const int ledGreen = 13;
+const int ledWhite = 7;
+const int ledYellow = 5;
+int n = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledYellow, OUTPUT);
+  pinMode(ledWhite, OUTPUT);
+  pinMode(ledGreen, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledGreen, HIGH);
+  delay(1500000);
+  digitalWrite(ledGreen, LOW);
+  for (int i = 1; i < 4; i++) { 
+    digitalWrite(ledWhite, HIGH);
+    delay(100);
+    digitalWrite(ledWhite, LOW);
+    delay(100);
+  }
+  digitalWrite(ledYellow, HIGH);
+  delay(300000);
+  digitalWrite(ledYellow, LOW);
+}
+
